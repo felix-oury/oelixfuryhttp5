1ff6b28f6f74adf7ac79308d60192e9330a6be33
“felix-oury” <felix.oury19@imperial.ac.uk>
2021-11-18 11:17:01 +0000

    addnewstuff6

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -10,7 +10,7 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("hello how are you");
+        resp.getWriter().write("hello");
         req.getServletPath();
     }
     @Override
